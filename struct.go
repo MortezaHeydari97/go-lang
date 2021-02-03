aa4fb6800d51568225aa00e882b6031600fbf10c
Morteza Heydari <mortezaheydarime@gmail.com>
2021-02-03 21:23:20 +0330

    Updates go lang strcut section

--- struct.go
+++ struct.go
@@ -17,4 +17,24 @@ func main() {
 	// You can access a struct field using a dot like so:
 	a.x = 5
 	fmt.Println("result =>", a)
+
+
+	// We also can access the struct fields using pointers.
+	v := Primary{10, 20}
+	p := &v
+	p.x = 1e3
+
+	fmt.Println("cumbersome", *&p.x) // We can use struct field using a pointer but this is not good at all
+	fmt.Println("new", p.x) // This is better to access struct field using pointer
+
+	// Struct literals 
+	v1 := Primary{7, 8} // x: 7, y: 8
+	v2 := Primary{x: 5} // x: 5, y: 0
+	v3 := Primary{} // x: 0, y: 0
+	v4 := &Primary{4, 5} // x
+
+	fmt.Println("a", v1)
+	fmt.Println("b", v2)
+	fmt.Println("c", v3)
+	fmt.Println("d", *v4)
 }
