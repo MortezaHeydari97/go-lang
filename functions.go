d8fea9fddc0c12780982b6ed1a200ebc8ba500cf
Morteza Heydari <mortezaheydarime@gmail.com>
2021-02-12 18:14:23 +0330

    Adds go lang closures to function section

--- functions.go
+++ functions.go
@@ -9,6 +9,16 @@ func compute(fn func(float64, float64) float64) float64 {
 	return fn(3, 4)
 }
 
+
+// closure
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
 func main() {
 	hypot := func(x, y float64) float64 {
 		return math.Sqrt(x + y)
@@ -16,4 +26,10 @@ func main() {
 	
 	fmt.Println("result => ", hypot(1.1, 2.3))
 	fmt.Println("result => ", compute(hypot))
+
+	pos, neg := adder(), adder()
+	
+	for i := 0 ;i < 10; i++ {
+		fmt.Println(pos(i), neg(-2*i))
+	}
 }
